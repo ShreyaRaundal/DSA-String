bb8a3b300c4be85c39a37ea58a546192968b377b
Shreya Raundal <136178072+ShreyaRaundal@users.noreply.github.com>
2025-06-13 09:07:57 +0530

    Add files via upload

---
+++ CheckSubString.java
@@ -0,0 +1,16 @@
+public class CheckSubString{
+    public static void main(String[] args){
+        String txt="geeksforgeeks";
+        String pat="eks";
+
+        int idx=txt.indexOf(pat);
+
+        if(idx!=-1){
+            System.out.println(idx);
+        }else{
+            System.out.println(idx);
+        }
+
+    }
+
+}
---
+++ Immutable.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Immutable {
+    public static void main(String[] args){
+        String s1="java";
+        s1.concat("rules");
+
+        System.out.println("s1 refers to "+s1);
+
+    }
+    
+}
