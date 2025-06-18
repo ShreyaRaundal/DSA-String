db5cd4849cd8310297d44c424099f1a2b04a3ee1
Shreya Raundal <136178072+ShreyaRaundal@users.noreply.github.com>
2025-06-19 04:06:08 +0530

    Add files via upload

---
+++ SubSequence.java
@@ -0,0 +1,28 @@
+public class SubSequence {
+    static boolean isSubsequence(String s1,String s2,int m,int n){
+        if(m==0){
+            return true;
+        }
+        if(n==0){
+            return false;
+        }
+        if(s1.charAt(m-1)==s2.charAt(n-1)){
+            return isSubsequence(s1, s2, m-1, n-1);
+        }
+        return isSubsequence(s1,s2,m,n-1);
+    }
+    public static void main(String[] args){
+        String s1="gksrek";
+        String s2="geeksforgeeks";
+        int m=s1.length();
+        int n=s2.length();
+
+        boolean res=isSubsequence(s1,s2,m,n);
+        if(res){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+
+    }
+}
